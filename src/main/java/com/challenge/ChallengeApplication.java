9099a682a92ff2481bedec14935c654747b0f660
AlanFarias97 <alanfarias_97@hotmail.com>
2024-03-18 20:51:59 -0300

    add rabbit config 8

--- src/main/java/com/challenge/ChallengeApplication.java
+++ src/main/java/com/challenge/ChallengeApplication.java
@@ -2,8 +2,12 @@ package com.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.challenge.persistence.model")
+@ComponentScan(basePackages = "com.challenge.config")
 public class ChallengeApplication {
 
 	public static void main(String[] args) {
