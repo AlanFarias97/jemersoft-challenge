325aa5287a29eb46223b00520c43e5377f3197bc
AlanFarias97 <alanfarias_97@hotmail.com>
2024-03-19 14:48:05 -0300

    set server swagger config

--- src/main/java/com/challenge/config/CorsConfig.java
+++ src/main/java/com/challenge/config/CorsConfig.java
@@ -1,7 +1,13 @@
 package com.challenge.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 public class CorsConfig {
 
@@ -12,5 +18,20 @@ public class CorsConfig {
                 registry.addMapping("/*").allowedOrigins("http://jemersoft-challenge-production.up.railway.app");
             }
         };
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowCredentials(true);
+        // the below three lines will add the relevant CORS response headers
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new
+                UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
